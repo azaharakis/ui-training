4665a8c97cb4c0c5931ec2339c988ec5c56ef555
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2016-12-23 10:28:15 +1100

    Add the ability to console log in the browser

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
+import React, {Component} from 'react';
+import {Link} from 'react-router';
 import './App.css';
 
 export default class App extends Component {
-  render() {
-    return (
-        <div>
-            <h2>Modules</h2>
-            <ul>
-                <li><Link to="/intro-to-es6">Introduction to ES 6</Link></li>
-            </ul>
-            {this.props.children}
-        </div>
-    );
-  }
+    render() {
+        document.getElementById('console-log-text').innerHTML = '';
+        return (
+            <div>
+                <h2>Modules</h2>
+                <ul>
+                    <li><Link to="/intro-to-es6">Introduction to ES 6</Link></li>
+                </ul>
+                {this.props.children}
+            </div>
+        );
+    }
 }
